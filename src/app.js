675ba09939ecd6ea082d561bd0089517f27f3568
nisarg kadliya <nisargkadliya@gmail.com>
2024-08-06 18:46:59 +0530

    commit

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 const express=require('express');
 const path=require('path');
 const hbs=require('hbs');
+const dotenv = require("dotenv");
+const session = require('express-session');
 
-
+const port = process.env.PORT;
+// console.log(port);
 const app=express();
+
+dotenv.config({path:'../.env'});
+require("./db/conn");
 
 // app.use(express.json());
 
@@ -27,6 +33,22 @@ app.set("view engine", "hbs");
 app.set("views", path2);
 
 hbs.registerPartials(path3);
+
+app.use(session({
+    secret: 'hello',
+    resave: false,
+    saveUninitialized: true
+}));
+
+var x=0;
+
+const middlware1 = (req,res,next) =>{
+    if(x==1) next();
+    else {
+        res.render("login");
+    }
+}
+
 
 app.get("/",(req,res)=>{
     res.render('index');
